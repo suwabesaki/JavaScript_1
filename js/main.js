d9eb34dc952ee1849a95f1522a9748b93d4d0d2d
saki.suwabe <saki.suwabe@gmail.com>
2020-06-08 10:02:09 +0900

    クラス名の変更とインデントの修正

--- js/main.js
+++ js/main.js
@@ -1,26 +1,22 @@
 'use strict';
 {
-  
-    const menu1 = document.getElementById('menu1');
-    const content1 = document.getElementById('contents1');
-    const menu2 = document.getElementById('menu2');
-    const content2 = document.getElementById('contents2');
-  
+  const menu1 = document.getElementById('menu1');
+  const content1 = document.getElementById('contents1');
+  const menu2 = document.getElementById('menu2');
+  const content2 = document.getElementById('contents2')
 
-    menu1.addEventListener("click",() =>{
-      menu1.classList.toggle("active");
-      });
-      menu1.addEventListener("click",() =>{
-        content1.classList.toggle("active");
-      });
+  menu1.addEventListener('click',() =>{
+    menu1.classList.toggle("upArrow");
+  });
+  menu1.addEventListener('click',() =>{
+    content1.classList.toggle("active");
+  });
 
-    menu2.addEventListener("click",() =>{
-      menu2.classList.toggle("active");
-      });
-      menu2.addEventListener("click",() =>{
-        content2.classList.toggle("active");
-      });
-     
-  
-  }
+  menu2.addEventListener('click',() =>{
+    menu2.classList.toggle("upArrow");
+  });
+  menu2.addEventListener('click',() =>{
+    content2.classList.toggle("active");
+  });
+}
   
